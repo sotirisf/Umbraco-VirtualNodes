ac0139a322f40fe9f6f2a443dce457a8ab1dc990
Sotiris Filippidis <pilotsf@hotmail.com>
2020-07-12 15:38:18 +0300

    nuspec file

--- VirtualNodes/Properties/AssemblyInfo.cs
+++ VirtualNodes/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VirtualNodes")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Makes Umbraco path segments invisible")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DotSee Web Services")]
 [assembly: AssemblyProduct("VirtualNodes")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © DotSee Web Services 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
